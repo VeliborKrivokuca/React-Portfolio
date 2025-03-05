88037d12368f7eeaff4a0a19dd262d97b1cd5f4c
Velibor <veliborkrivokuca1@gmail.com>
2025-03-05 23:03:03 +0100

    Reverted to using react reveal

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -5,35 +5,35 @@ import WorkCard from "../components/WorkCard";
 import { WorkData, WorkCardData } from "../data/WorkData";
 
 function Work() {
-    return (
-        <>
-            <section id="work">
-                <div className="work container">
-                    <Fade bottom cascade>
-                        <div className="work_head">
-                            <span>{WorkData.work_head_tag}</span>
-                            <h1>{WorkData.work_head_title}</h1>
-                        </div>
-                    </Fade>
+  return (
+    <>
+      <section id="work">
+        <div className="work container">
+          <Fade bottom cascade>
+            <div className="work_head">
+              <span>{WorkData.work_head_tag}</span>
+              <h1>{WorkData.work_head_title}</h1>
+            </div>
+          </Fade>
 
-                    <Fade>
-                        <div className="work_content row">
-                            {WorkCardData.map((val) => {
-                                return (
-                                    <WorkCard
-                                        key={val.id}
-                                        icon={val.work_icon}
-                                        name={val.work_name}
-                                        desc={val.work_desc}
-                                    />
-                                );
-                            })}
-                        </div>
-                    </Fade>
-                </div>
-            </section>
-        </>
-    );
+          <Fade>
+            <div className="work_content row">
+              {WorkCardData.map((val) => {
+                return (
+                  <WorkCard
+                    key={val.id}
+                    icon={val.work_icon}
+                    name={val.work_name}
+                    desc={val.work_desc}
+                  />
+                );
+              })}
+            </div>
+          </Fade>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Work;
