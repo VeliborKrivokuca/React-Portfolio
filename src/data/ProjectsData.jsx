723b99a408436c850d2c1495724405ee6a980a32
Velibor <veliborkrivokuca1@gmail.com>
2025-03-13 21:01:01 +0100

    Added CV File, edited styling - margins

--- src/data/ProjectsData.jsx
+++ src/data/ProjectsData.jsx
@@ -1,22 +1,39 @@
-import { CgUserlane } from "react-icons/cg";
-import {
-} from "react-icons/io5";
+import { CgAbstract, CgBot, CgBowl, CgUserlane } from "react-icons/cg";
+import {} from "react-icons/io5";
 
 const ProjectsData = {
-    projects_head_tag: "Portfolio",
-    projects_head_title: "Some of my cool projects",
+  projects_head_tag: "Portfolio",
+  projects_head_title: "Some of my cool projects",
 };
 
 const ProjectsCardData = [
-    // {
-    //     id: 1,
-    //     project_img: "assets/projects/web1.png",
-    //     project_alt: "Web 1",
-    //     project_icon: <CgUserlane />,
-    //     project_title: "Portfolio",
-    //     project_link: "https://velibor.in/",
-    //     project_bg: "info info_dark",
-// },
+  {
+    id: 1,
+    project_img: "assets/projects/web1.png",
+    project_alt: "Web 1",
+    project_icon: <CgUserlane />,
+    project_title: "Portfolio",
+    project_link: "https://velibor-react-portfolio.vercel.app/",
+    project_bg: "info info_dark",
+  },
+  {
+    id: 2,
+    project_img: "assets/projects/web2.png",
+    project_alt: "Web 2",
+    project_icon: <CgBowl />,
+    project_title: "Culinary",
+    project_link: "https://culinary-qapg.vercel.app/",
+    project_bg: "info info_dark",
+  },
+  {
+    id: 2,
+    project_img: "assets/projects/web3.png",
+    project_alt: "Web 3",
+    project_icon: <CgBot />,
+    project_title: "OPEN AI Landing Page",
+    project_link: "https://open-ai-zom5.vercel.app/",
+    project_bg: "info info_dark",
+  },
 ];
 
 export { ProjectsData, ProjectsCardData };
--- src/pages/Biodata.jsx
+++ src/pages/Biodata.jsx
@@ -3,42 +3,45 @@ import "../css/Biodata.css";
 import Timeline from "../components/Timeline";
 import { BioSkillsIcons } from "../components/BioDataCard";
 import { BioDataHead, BioDataSkills } from "../data/BioData";
+// Import your CV file (adjust the path and filename as necessary)
+import CV from "../assets/projects/velibor-krivokuca-cv.pdf";
 
 function Biodata() {
-    return (
-        <>
-            <section id="biodata">
-                <div className="biodata container">
-                    <div className="biodata_head">
-                        <span>{BioDataHead.bio_head_tag}</span>
-                        <h1>{BioDataHead.bio_head_title}</h1>
-                    </div>
+  return (
+    <>
+      <section id="biodata">
+        <div className="biodata container">
+          <div className="biodata_head">
+            <span>{BioDataHead.bio_head_tag}</span>
+            <h1>{BioDataHead.bio_head_title}</h1>
+          </div>
 
-                    <div className="biodata_timeline row">
-                        <Timeline />
-                    </div>
+          <div className="biodata_timeline row">
+            <Timeline />
+          </div>
 
-                    <div className="biodata_skills">
-                        <h2>{BioDataHead.bio_skills_sub_head}</h2>
-                        <div className="skills_wrapper">
-                            {BioDataSkills.map((val) => {
-                                return (
-                                    <BioSkillsIcons
-                                        key={val.id}
-                                        icon={val.skills_icon}
-                                        alt={val.skills_alt}
-                                    />
-                                );
-                            })}
-                        </div>
-                        <button className="btn">
-                            {BioDataHead.bio_btn_1_text}
-                        </button>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+          <div className="biodata_skills">
+            <h2>{BioDataHead.bio_skills_sub_head}</h2>
+            <div className="skills_wrapper">
+              {BioDataSkills.map((val) => {
+                return (
+                  <BioSkillsIcons
+                    key={val.id}
+                    icon={val.skills_icon}
+                    alt={val.skills_alt}
+                  />
+                );
+              })}
+            </div>
+            {/* Use an anchor tag with download attribute for the CV download */}
+            <a href={CV} download="CV.pdf" className="btn download-btn">
+              {BioDataHead.bio_btn_1_text}
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default Biodata;
